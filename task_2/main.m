05dd804c1149c780e98f6ce21bfd47c544fc668a
Stephen Adesina <bauerstevenson@gmail.com>
2022-09-19 14:12:41 +0300

    fixed issues

--- task_2/main.m
+++ task_2/main.m
@@ -14,6 +14,15 @@ h = 0.5;
 vel = velocity(y,n,5)
 acc = acceleration(y,n,5)
 
+subplot(1,2,1)
 plot(x,vel,'bo-')
+xlabel('time(s)')
+ylabel('velocity(m/s)')
+title('Velocity-Time graph')
 
+subplot(1,2,2)
+plot(x,acc,'bo-')
+xlabel('time(s)')
+ylabel('acceleration(m/s^2)')
+title('Acceleration-Time graph')
 
