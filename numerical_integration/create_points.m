4103afbd1d8875c7fbcc245895a5f555e09a6372
Stephen Adesina <bauerstevenson@gmail.com>
2022-10-27 17:11:53 +0300

    Delete numerical_integration directory

--- numerical_integration/create_points.m
+++
@@ -1,8 +0,0 @@
-function res = create_points(func,a,b)
-res = zeros(b-a);
-    for i=a:b
-    res(i,1) = i;
-    res(i,2) = func(i);
-    end
-   
-end
--- numerical_integration/task_1/main.m
+++
@@ -1,9 +0,0 @@
-clc
-
-clear
-
-close all
-
-func = @(y) (4*pi*y/25)*sqrt(9-y^2);
-f0 = createpoints(func,-5,5)
-trapz(f0)
