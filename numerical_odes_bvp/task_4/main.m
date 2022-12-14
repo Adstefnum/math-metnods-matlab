c43af66dec97d044295a34ed4dbafe5a7306f1cd
Stephen Adesina <bauerstevenson@gmail.com>
2022-12-14 08:43:20 +0200

    Update main.m

--- numerical_odes_bvp/task_4/main.m
+++ numerical_odes_bvp/task_4/main.m
@@ -3,6 +3,7 @@ clc
 clear
 
 close all
+format short g
 
  hc = 40;
 P = 0.016;
@@ -10,6 +11,13 @@ k = 240;
 Ac = 1.6 * 10e-5;
 Ts = 293;
 
-func =  @(x,Y)[Y(2);((Y(1)-Ts).*(hc*P)/(k*Ac))];
 
- [rx,ry] = runge_kutta_4th(func,[0,0.1])
+
+
+row_funcs = @(r,h) [ 1, -(2+(h^2*hc*P)/(k*Ac)),1];
+b_func = @(h) (-h^2*hc*P)/(k*Ac);
+res = calc_bvp([0,0.1],100,[473,293],row_funcs,b_func)
+plot(res(:,1),res(:,2))
+
+
+
