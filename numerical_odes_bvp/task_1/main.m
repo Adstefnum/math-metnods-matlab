99bf8f0b180e140b838e032d3f25f865908df22f
Stephen Adesina <bauerstevenson@gmail.com>
2022-12-14 08:32:06 +0200

    Update main.m

--- numerical_odes_bvp/task_1/main.m
+++ numerical_odes_bvp/task_1/main.m
@@ -8,34 +8,10 @@ format short g
 
 
 
-a = 1;
-b = 3.5;
-n = 100;
-h = (b-a)/n;
-A = zeros(n+1,n+1);
-A(1,1)=1;
-A(n+1,n+1) = 1;
-r = 1;
-j = 1;
-for i=2:n+1
-    r = r+h;
-   
-    if j+2 <= n+1
-         A(i,j) = (r);
-            
-        A(i,j+1) = (-(2*r+h));
-        
-        A(i,j+2)= ((r+h));        
-    end
-    j = j+1;
-end
-b=-500*h^2*ones(n+1,1);
-
-b(1) = 600;
-b(n+1) = 25;
-x=linspace(1,3.5,n+1);
-y=A\b
-plot(x,y)
+row_funcs = @(r,h) [ r, -(2*r+h),r+h];
+b_func = @(h) -500*h^2;
+res = calc_bvp([1,3.5],100,[600,25],row_funcs,b_func)
+plot(res(:,1),res(:,2))
 
 
- 
+
