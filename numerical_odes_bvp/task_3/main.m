844893e51ca43525474e3b112f2e53f387a83d38
Stephen Adesina <bauerstevenson@gmail.com>
2022-12-14 10:37:22 +0200

    Update main.m

--- numerical_odes_bvp/task_3/main.m
+++ numerical_odes_bvp/task_3/main.m
@@ -1,11 +1,22 @@
 clc
-
-clear
-
+clear all
 close all
-m =2;
-k =10;
+x(1) = 0.01; % initial position, meters
+v(1) = 0; % initial velocity, m/s
+Deltat = 0.5; % time increment, s
+x(2) = x(1) + v(1)*Deltat; 
 
+k = 10; % Newtons / meter
+m = 2; % kilograms
 
+Tfinal = 8.0; % ending time, seconds
+t = 0:Deltat:Tfinal; 
+N = length(t); 
+for j=3:N;
+ x(j) = 2*x(j-1) - x(j-2) + Deltat*Deltat*(-1.0*k/m)*x(j-1);
+ v(j-1) = (x(j) - x(j-2))/ (2*Deltat);
+end
 
- [rx,ry] = verlet_velocity(func,[0,8],[600 0 0 0 0 25],0.5,2)
+v(N) = (x(N)-x(N-1))/Deltat; 
+figure; plot(t, x, 'ko:'); grid on; 
+xlabel('Time, sec. ')
